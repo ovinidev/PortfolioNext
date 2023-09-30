bc476c6ad9e1a63e1d46d5c8bfbf79c78de3b026
ovinidev <vinimedeiros7@outlook.com>
2023-09-30 20:05:43 -0300

    feat: change image

--- src/app/youtube/[slug]/page.tsx
+++ src/app/youtube/[slug]/page.tsx
@@ -1,10 +1,8 @@
-import { PageProps } from "../../../../.next/types/app/layout";
-
 interface ReposResponse {
   name: string;
 }
 
-export default async function Page({ params }: PageProps) {
+export default async function Page({ params }: { params: { slug: string } }) {
   const response = await fetch(
     `https://api.github.com/users/${params.slug}/repos`,
   );
